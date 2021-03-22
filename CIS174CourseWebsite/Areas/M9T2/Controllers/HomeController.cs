132860b296332875c0a259a2529c61a7bf60799a
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-03-22 14:10:41 -0500

    Added new M9T2 area with base files.

---
+++ CIS174CourseWebsite/Areas/M9T2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿/***************************************************************
+* Name        : M9T2\HomeController.cs
+* Author      : Tom Sorteberg
+* Created     : 03/22/2021
+* Course      : CIS 174
+* Version     : 1.0
+* OS          : Windows 10 Pro, Visual Studio Community 2019
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Module 9 Topic 2
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access 
+* to my program.         
+***************************************************************/
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174CourseWebsite.Areas.M9T2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CIS174CourseWebsite/Migrations/Ticket/20210315094057_m8t2azure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CIS174CourseWebsite.Migrations.Ticket
+{
+    public partial class m8t2azure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
